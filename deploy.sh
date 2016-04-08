f5d3e4c11cf1d23318292e933961e715840d2022
nikashitsa <vernik91@gmail.com>
2016-04-09 02:29:27 +0600

    Move out nginx proxy

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,6 @@ echo "=== Docker-machine: $DOCKER_MACHINE_NAME"
 echo "=== ENV: $ENV"
 
 rm -rf build
-export $(cat .env.$APP) && echo $DB_HOST && npm run build
+export $(cat .env.$ENV) && echo $DB_HOST && npm run build
 docker-compose build web
 docker-compose -f docker-compose.yml -f docker-compose.$ENV.yml up -d
-
-# Start proxy, if missing
-NGINX_PROXY=$(docker ps -fq name=nginx-proxy)
-if [ -z $NGINX_PROXY ]; then
-  docker-compose -f docker-nginx-proxy.yml up -d
-fi
