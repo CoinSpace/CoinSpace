6384f1506a3080da26f074a7a357a91ffe7fc689
nikashitsa <vernik91@gmail.com>
2016-04-14 16:50:52 +0600

    Fix href

--- app/widgets/terms/index.js
+++ app/widgets/terms/index.js
@@ -4,20 +4,21 @@ var Ractive = require('cs-ractive')
 var emitter = require('cs-emitter')
 var $ = require('browserify-zepto')
 
-module.exports = function(el){
-    var ractive = new Ractive({
-        el: el,
-        template: require('./index.ract').template
-    })
+module.exports = function (el) {
+  var ractive = new Ractive({
+    el: el,
+    template: require('./index.ract').template
+  })
 
-    ractive.on('back', function(e){
-        setTimeout(function(){
-            $("#terms").addClass('closed')
-            $("#sidebar").addClass('open')
-            $("#main").removeClass('terms-open');
-            $("#terms").removeClass('terms-open');
-        }, 0)
-    })
+  ractive.on('back', function (e) {
+    e.original.preventDefault()
+    setTimeout(function () {
+      $('#terms').addClass('closed')
+      $('#sidebar').addClass('open')
+      $('#main').removeClass('terms-open')
+      $('#terms').removeClass('terms-open')
+    }, 0)
+  })
 
-    return ractive
+  return ractive
 }
