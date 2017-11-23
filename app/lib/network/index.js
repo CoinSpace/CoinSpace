a726d3d0fa8e95935ccebaacc9d2dd6f24d0eff8
nikashitsa <vernik91@gmail.com>
2017-11-23 21:21:47 +0600

    Add last token safe-redirect

--- app/lib/network/index.js
+++ app/lib/network/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function(defaultNetwork) {
+  if (typeof defaultNetwork === 'undefined') {
+    defaultNetwork = 'bitcoin';
+  }
+
   var regex = /^network=/
   var networkParam = window.location.search.substr(1).split('&').filter(function(e){
     return e.match(regex)
   })[0]
 
-  return networkParam ? networkParam.replace(regex, '') : 'bitcoin'
+  return networkParam ? networkParam.replace(regex, '') : defaultNetwork;
 }
--- app/loader/index.js
+++ app/loader/index.js
@@ -2,30 +2,47 @@
 
 require('../application.scss');
 
-var token = require('lib/network')()
+var token = require('lib/network')('')
 var fadeOut = require('lib/transitions/loader').out
 var Modernizr = require('modernizr')
 var i18n = require('lib/i18n')
 
-document.getElementsByTagName('html')[0].classList.add(token)
-var containerEl = document.getElementById('loader')
+function init() {
+  var lastToken = window.localStorage.getItem('_cs_token');
+  window.localStorage.setItem('_cs_token', token);
 
-Modernizr.on('indexeddb', function(hasIndexedDB){
-  var supportsPouchDB = hasIndexedDB || Modernizr.websqldatabase
-
-  i18n.loadTranslation().then(function() {
-    if (supportsPouchDB && Modernizr.localstorage && Modernizr.webworkers && Modernizr.blobconstructor && Modernizr.getrandomvalues) {
-      return import(
-        /* webpackChunkName: 'application' */
-        '../application'
-      ).then(function() {
-        fadeOut(containerEl)
-      });
-    } else {
-      return import(
-        /* webpackChunkName: 'nope' */
-        './nope'
-      );
+  if (!token) {
+    var baseUrl = window.location.href.split('?')[0];
+    var url = baseUrl + '?network=' + lastToken;
+    if (!lastToken) {
+      url = baseUrl + '?network=bitcoin';
     }
+    window.location.assign(url);
+    return false;
+  }
+
+  document.getElementsByTagName('html')[0].classList.add(token)
+  var containerEl = document.getElementById('loader')
+
+  Modernizr.on('indexeddb', function(hasIndexedDB){
+    var supportsPouchDB = hasIndexedDB || Modernizr.websqldatabase
+
+    i18n.loadTranslation().then(function() {
+      if (supportsPouchDB && Modernizr.localstorage && Modernizr.webworkers && Modernizr.blobconstructor && Modernizr.getrandomvalues) {
+        return import(
+          /* webpackChunkName: 'application' */
+          '../application'
+        ).then(function() {
+          fadeOut(containerEl)
+        });
+      } else {
+        return import(
+          /* webpackChunkName: 'nope' */
+          './nope'
+        );
+      }
+    })
   })
-})
+}
+
+init();
