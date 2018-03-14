eed869c609839dc83248ad91e3732a23bf70b0ef
nikashitsa <vernik91@gmail.com>
2018-03-15 03:05:03 +0600

    Update tokens page

--- app/pages/tokens/index.js
+++ app/pages/tokens/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Ractive = require('lib/ractive');
-var getNetwork = require('lib/network');
 var showRemoveConfirmation = require('widgets/modals/confirm-remove-token');
 var addEthereumToken = require('widgets/modals/add-ethereum-token');
 
@@ -18,49 +17,33 @@ module.exports = function(el) {
     data: {
       title: 'Available Tokens',
       id: 'token_dropdown',
+      currentToken: 'ethereum', // string or object
+      isCurrentToken: function(token) {
+        return this.get('currentToken') === token;
+      },
+      switchToken: switchToken,
+      removeEthereumToken: removeEthereumToken,
       ethereumTokens: tokens.filter(function(token) {
         return token.network === 'ethereum'
       }),
-      isCurrentToken: isCurrentToken
     }
   })
 
-  function isCurrentToken(network, tokenId) {
-    // currentToken should belong to currentNetwork or be undefined;
-    // var currentToken = {address: '0x00000', name: 'EOS', symbol: 'EOS', decimals: 18, network: 'ethereum'};
-    var currentToken;
-    var currentNetwork = getNetwork();
-    if (currentNetwork !== network) return false;
-    if (currentNetwork === 'ethereum' && currentToken) {
-      return tokenId === currentToken.address;
-    }
-    return tokenId === undefined;
+  function switchToken(token) {
+    if (token === ractive.get('currentToken')) return;
+    ractive.set('currentToken', token);
   }
 
-  ractive.on('switch', function(context) {
-    var network = context.node.dataset.network;
-    var tokenId = context.node.dataset.tokenId;
-    if (isCurrentToken(network, tokenId)) return;
-    console.log('Switch to network:' + network + ' tokenId: ' + tokenId);
-    // var url = window.location.href.replace(/\?network=\w+/, '') + '?network=' + token
-    // window.location.assign(url);
-  });
-
-  ractive.on('add-ethereum-token', function() {
-    console.log('add-ethereum-token');
-    addEthereumToken();
-  });
-
-  ractive.on('remove-ethereum-token', function(context) {
-    var ethereumTokens = ractive.get('ethereumTokens');
-    var token = ethereumTokens.filter(function(item) {
-      return item.address === context.node.dataset.tokenId;
-    })[0];
-    var index = ethereumTokens.indexOf(token);
+  function removeEthereumToken(token) {
+    var index = ractive.get('ethereumTokens').indexOf(token);
     showRemoveConfirmation(token, tokens, function() {
       ractive.splice('ethereumTokens', index, 1);
     });
     return false;
+  }
+
+  ractive.on('add-ethereum-token', function() {
+    addEthereumToken();
   });
 
   return ractive
