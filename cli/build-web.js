34f98bb81a27264dcedaf8fc39e6e05444fd6655
nikashitsa <vernik91@gmail.com>
2020-03-11 09:35:37 +0600

    Reduce cli build web

--- cli/build-web.js
+++ cli/build-web.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-var utils = require('./utils');
 var warning = require('chalk').yellow;
 var webpack = require('webpack');
 
 program
   .name('build-web.js')
   .option('-e, --env <env>', 'environment', 'dev')
-  .option('--scale <number>', 'scale web containers')
-  .option('--no-deploy', 'no deploy')
   .parse(process.argv);
 
 var currentDockerMachine = process.env.DOCKER_MACHINE_NAME ? process.env.DOCKER_MACHINE_NAME : 'local';
@@ -25,16 +22,6 @@ var webpackConfig = require('../webpack.prod');
 webpack(webpackConfig, function(error, stats) {
   if (error) return console.error(error);
   if (stats.hasErrors()) return console.log(stats.toString({colors: true}));
-  if (!program.deploy) {
-    return console.log('Done! Deploy skipped.')
-  }
-
-  utils.shell('docker-compose build web');
-
-  var config = program.env !== 'loc' ? `-f docker-compose.${program.env}.yml` : '';
-  var scale = program.scale ? `--scale web=${program.scale}` : '';
-
-  utils.shell(`docker-compose ${config} up -d ${scale}`);
   console.log('Done!');
 });
 
