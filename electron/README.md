190b99a7419c34dd9472ac8d06133bd2732fed88
Evgeny Vlasenko <mahnunchik@gmail.com>
2020-04-28 17:21:33 +0200

    electron build moved to separate folder

--- electron/README.md
+++ electron/README.md
@@ -1,5 +1,3 @@
-1. In root folder ```./cli/build.js electron``` to build web-app in build folder
-2. In root folder ```./cli/build.js electron --run``` - to run and debug electron app
-    2.1 Or ```npm run electron``` to run electron without build web-app
-3. In root folder ```./cli/build.js electron --release``` to compile electron app tot electron/dist folder
-    3.1 Or ```npm run electron-builder``` to compile electron without build web-app
+# Coin Wallet
+
+The Coin Wallet desktop application.
--- electron/electron.js
+++ electron/electron.js
@@ -2,37 +2,29 @@
 const { app, shell, BrowserWindow } = require('electron');
 const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
 // TODO add autoupdater
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 500,
     height: 700,
     minWidth: 500,
     minHeight: 700,
     webPreferences: {
-      devTools: false
-    }
+      devTools: false,
+    },
   });
 
-  let mainSession = mainWindow.webContents.session;
-
-  // TODO add check that index.html exist in build
-  // and load the index.html of the app.
-  mainWindow.loadFile('./electron/build/index.html');
+  mainWindow.loadFile('./build/index.html');
 
   // set api.moonpay.io cookies
-  mainSession.cookies.set({
+  mainWindow.webContents.session.cookies.set({
     url: 'https://api.moonpay.io',
     name: 'customerToken',
     value: '',
     domain: 'api.moonpay.io',
-    expirationDate: 9999999999
+    expirationDate: 9999999999,
   });
 
   // to see all cookies in console
@@ -48,14 +40,6 @@ function createWindow() {
     event.preventDefault();
     shell.openExternal(url);
   });
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
 }
 
 // This method will be called when Electron has finished
@@ -67,13 +51,17 @@ app.on('ready', createWindow);
 app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function() {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
