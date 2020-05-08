4f784f5f2f70fec7b47698b4cf7e1255eb7852fb
nikashitsa <vernik91@gmail.com>
2020-05-08 17:43:04 +0600

    Fix tooltip

--- app/widgets/modals/tooltip/index.js
+++ app/widgets/modals/tooltip/index.js
@@ -4,10 +4,6 @@ var Ractive = require('widgets/modals/base')
 
 module.exports = function showTooltip(data){
 
-  if (process.env.BUILD_TYPE === 'phonegap') {
-    data.bottomLink = false
-  }
-
   var ractive = new Ractive({
     el: document.getElementById('tooltip'),
     partials: {
