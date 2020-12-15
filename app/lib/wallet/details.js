f60b766e80f8055635f50c3196a423b4eeb0e938
Evgeny Vlasenko <mahnunchik@gmail.com>
2020-12-15 11:57:50 +0300

    fix(app): mistype in details

--- app/lib/wallet/details.js
+++ app/lib/wallet/details.js
@@ -63,7 +63,7 @@ async function _initDetails() {
       symbol: 'USDT',
       address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
       decimals: 6,
-      platform: 'ethereum',
+      network: 'ethereum',
     }],
   };
 
