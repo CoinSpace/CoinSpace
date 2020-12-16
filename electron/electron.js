653117478d7040b8f5f96e288598ccbfbdb77e15
Evgeny Vlasenko <mahnunchik@gmail.com>
2020-12-16 15:31:10 +0300

    fix(electron): window recreation on macos

--- electron/electron.js
+++ electron/electron.js
@@ -20,7 +20,6 @@ if (process.env.NODE_ENV === 'production') {
 
 if (require('electron-squirrel-startup')) return;
 
-
 // Suppress deprecation warning
 app.allowRendererProcessReuse = true;
 
@@ -39,73 +38,66 @@ if (isLinux) {
   });
 }
 
-function init() {
-  const lock = app.requestSingleInstanceLock();
 
-  // https://github.com/electron/electron/issues/15958
-  if (!isMas && !lock) {
-    app.quit();
-    return;
-  }
+const lock = app.requestSingleInstanceLock();
 
-  // Init crashReporter
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENVIRONMENT,
-    release: process.env.RELEASE,
+// https://github.com/electron/electron/issues/15958
+if (!isMas && !lock) {
+  app.quit();
+}
+// Init crashReporter
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT,
+  release: process.env.RELEASE,
+});
+
+// Set up Application Menu
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
+
+if (!app.isDefaultProtocolClient('coinspace')) {
+  // Define custom protocol handler. Deep linking works on packaged versions of the application!
+  app.setAsDefaultProtocolClient('coinspace');
+}
+
+// The application has finished basic startup
+app.on('will-finish-launching', () => {
+  // Protocol handler for macOS
+  app.on('open-url', (event, url) => {
+    event.preventDefault();
+    openWindow(url);
   });
+});
 
-  // Set up Application Menu
-  const menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
+// Someone tried to run a second instance
+app.on('second-instance', (event, argv) => {
+  openWindow(argv.find(item => item.startsWith('coinspace')));
+});
 
-  if (!app.isDefaultProtocolClient('coinspace')) {
-    // Define custom protocol handler. Deep linking works on packaged versions of the application!
-    app.setAsDefaultProtocolClient('coinspace');
-  }
-
-  // TODO add autoupdater
-
-  // The application has finished basic startup
-  app.on('will-finish-launching', () => {
-    // Protocol handler for macOS
-    app.on('open-url', (event, url) => {
-      event.preventDefault();
-      openWindow(url);
-    });
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  protocol.registerStringProtocol('coinspace', (request, cb) => {
+    openWindow(request.url);
+    cb('ok');
   });
-
-  // Someone tried to run a second instance
-  app.on('second-instance', (event, argv) => {
-    openWindow(argv.find(item => item.startsWith('coinspace')));
-  });
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', () => {
-    protocol.registerStringProtocol('coinspace', (request, cb) => {
-      openWindow(request.url);
-      cb('ok');
-    });
-    openWindow();
-    updater({ log });
-  });
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
+  openWindow();
+  updater({ log });
 
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     openWindow();
   });
-}
+});
 
-init();
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
