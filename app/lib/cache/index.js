098b21eb34d17d4ebf06385a33338836704a1495
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-04-16 14:41:41 +0300

    feat(app): cache implementation

---
+++ app/lib/cache/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { encrypt, decrypt } = require('lib/encryption');
+const request = require('lib/request');
+
+class Cache {
+  constructor(baseUrl, name, key) {
+    this.url = `${baseUrl}api/v2/cache/${name}`;
+    this.key = key;
+    // no await
+    this.pending = this.init();
+  }
+  async init() {
+    const res = await request({
+      url: this.url,
+      method: 'get',
+      seed: 'public',
+    });
+    if (res.data) {
+      this.cache = JSON.parse(decrypt(res.data, this.key));
+    } else {
+      this.cache = {};
+    }
+  }
+  async get(key) {
+    await this.pending;
+    return this.cache[key];
+  }
+  async set(key, value) {
+    // no await
+    this.pending = this.pending
+      .then(async () => {
+        this.cache[key] = value;
+        const res = await request({
+          url: this.url,
+          method: 'put',
+          data: {
+            data: encrypt(JSON.stringify(this.cache), this.key),
+          },
+          seed: 'public',
+        });
+        this.cache = JSON.parse(decrypt(res.data, this.key));
+      });
+    return this.pending;
+  }
+}
+
+module.exports = Cache;
