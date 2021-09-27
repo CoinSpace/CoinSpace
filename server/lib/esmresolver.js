1f91d8a058157b3bf2428387216f297efafdef3d
Evgeny Vlasenko <mahnunchik@gmail.com>
2021-09-27 22:18:54 +0700

    fix(server): rewrite esmresolver

--- server/lib/esmresolver.js
+++ server/lib/esmresolver.js
@@ -9,25 +9,26 @@ export default function resolver(handlersPath, route, apiDoc) {
   const oId = schema['x-eov-operation-id'] || schema['operationId'];
   const baseName = schema['x-eov-operation-handler'];
 
-  const cacheKey = `${expressRoute}-${method}-${baseName}`;
+  if (oId && !baseName) {
+    // eslint-disable-next-line max-len
+    throw Error(`found x-eov-operation-id for route [${method} - ${expressRoute}]. x-eov-operation-handler required.`);
+  }
+  if (!oId && baseName) {
+    // eslint-disable-next-line max-len
+    throw Error(`found x-eov-operation-handler for route [${method} - ${expressRoute}]. x-eov-operation-id required.`);
+  }
+  if (!oId && !baseName) {
+    throw Error(`x-eov-operation-handler and x-eov-operation-id required for route [${method} - ${expressRoute}].`);
+  }
 
-  if (!handlersCache[cacheKey]) {
-    if (oId && !baseName) {
-      // eslint-disable-next-line max-len
-      throw Error(`found x-eov-operation-id for route [${method} - ${expressRoute}]. x-eov-operation-handler required.`);
-    }
-    if (!oId && baseName) {
-      // eslint-disable-next-line max-len
-      throw Error(`found x-eov-operation-handler for route [${method} - ${expressRoute}]. x-eov-operation-id required.`);
-    }
-    if (oId && baseName && typeof handlersPath === 'string') {
-      const modulePath = path.join(handlersPath, `${baseName}.js`);
-      handlersCache[cacheKey] = import(modulePath);
-    }
+  const modulePath = path.join(handlersPath, `${baseName}.js`);
+
+  if (!handlersCache[modulePath]) {
+    handlersCache[modulePath] = import(modulePath);
   }
 
   return (req, res, next) => {
-    handlersCache[cacheKey]
+    handlersCache[modulePath]
       .then((module) => {
         if (!module[oId]) {
           // eslint-disable-next-line max-len
