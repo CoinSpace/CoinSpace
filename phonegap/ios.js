54c4e4b0c1b775f8c14167c44f5eb34f5d4d4d25
nikashitsa <vernik91@gmail.com>
2021-10-09 00:17:02 +0200

    chore: update cs-watchapp-ios

--- phonegap/ios.js
+++ phonegap/ios.js
@@ -109,7 +109,7 @@ function updatePlist(plistPath, update) {
 
 async function addWatchApp() {
   utils.shell('git clone https://github.com/CoinSpace/cs-watchapp-ios.git', { cwd: buildPath });
-  utils.shell('cd cs-watchapp-ios && git checkout ea2bcbfac395f57d2d8493bf9224c1cfabb146e4 -q', { cwd: buildPath });
+  utils.shell('cd cs-watchapp-ios && git checkout eac7e7f743ccfad690e199b1fa41d7c1b691a9b5 -q', { cwd: buildPath });
   utils.shell(`ln -s ${path.resolve(buildPath, 'cs-watchapp-ios/WatchApp')} ./platforms/ios/WatchApp`, { cwd: buildPath });
   utils.shell(`ln -s ${path.resolve(buildPath, 'cs-watchapp-ios/WatchAppExtension')} ./platforms/ios/WatchAppExtension`, { cwd: buildPath });
 
