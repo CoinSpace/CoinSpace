330785c05e80175ef7d3d290b5194a55d16dda3a
nikashitsa <vernik91@gmail.com>
2022-06-02 23:31:32 +0200

    chore: fix ci (plugins)

--- phonegap/android.js
+++ phonegap/android.js
@@ -26,20 +26,20 @@ async function run() {
 
   /* eslint-disable max-len */
   cordova('platform add android@10.1.2 --save');
-  cordova('plugin add cordova-plugin-androidx-adapter@1.1.3');
-  cordova('plugin add cordova-custom-config@5.1.0');
-  cordova('plugin add cordova-plugin-geolocation@4.0.2');
-  cordova('plugin add phonegap-plugin-barcodescanner@8.1.0 --variable ANDROID_SUPPORT_V4_VERSION=28.0.0');
-  cordova('plugin add cordova-plugin-dialogs@2.0.2');
-  cordova('plugin add cordova-plugin-inappbrowser@4.0.0');
-  cordova('plugin add cordova-plugin-x-socialsharing@5.6.8');
-  cordova('plugin add cordova-plugin-fingerprint-aio@5.0.1');
-  cordova('plugin add cordova-plugin-customurlscheme@5.0.1 --variable URL_SCHEME=coinspace');
-  cordova('plugin add https://github.com/CoinSpace/cordova-plugin-zendesk#4bf28de7fcd6759450f5fd56f2ec28677bc882da');
-  cordova('plugin add cordova-plugin-splashscreen@5.0.4');
-  cordova('plugin add cordova-plugin-safariviewcontroller@1.6.0');
-  cordova('plugin add cordova-plugin-inapp-review@1.1.0 --variable PLAY_CORE_VERSION=1.8.0');
-  cordova('plugin add cordova-plugin-velda-devicefeedback@0.0.2');
+  cordova('plugin add cordova-plugin-androidx-adapter@1.1.3 --save');
+  cordova('plugin add cordova-custom-config@5.1.0 --save');
+  cordova('plugin add cordova-plugin-geolocation@4.0.2 --save');
+  cordova('plugin add phonegap-plugin-barcodescanner@8.1.0 --save --variable ANDROID_SUPPORT_V4_VERSION=28.0.0');
+  cordova('plugin add cordova-plugin-dialogs@2.0.2 --save');
+  cordova('plugin add cordova-plugin-inappbrowser@4.0.0 --save');
+  cordova('plugin add cordova-plugin-x-socialsharing@5.6.8 --save');
+  cordova('plugin add cordova-plugin-fingerprint-aio@5.0.1 --save');
+  cordova('plugin add cordova-plugin-customurlscheme@5.0.1 --save --variable URL_SCHEME=coinspace');
+  cordova('plugin add https://github.com/CoinSpace/cordova-plugin-zendesk#4bf28de7fcd6759450f5fd56f2ec28677bc882da --save');
+  cordova('plugin add cordova-plugin-splashscreen@5.0.4 --save');
+  cordova('plugin add cordova-plugin-safariviewcontroller@1.6.0 --save');
+  cordova('plugin add cordova-plugin-inapp-review@1.1.0 --save --variable PLAY_CORE_VERSION=1.8.0');
+  cordova('plugin add cordova-plugin-velda-devicefeedback@0.0.2 --save');
 
   if (process.env.RELEASE) {
     cordova('build android --release -- --packageType=apk');
