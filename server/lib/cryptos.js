52701fa129db1bedbb2ba6631f2353ae800f1f71
Evgeny Vlasenko <mahnunchik@gmail.com>
2023-03-31 16:59:14 +0400

    fix(server): slow logs

--- server/lib/cryptos.js
+++ server/lib/cryptos.js
@@ -59,75 +59,77 @@ async function updatePrices() {
   const PER_PAGE = 500;
   let page = 0;
   let cryptos;
-  do {
-    logs.push(`crypto update prices - load from db (chunk #${page}) - start`);
-    cryptos = await db.collection(COLLECTION)
-      .aggregate([{
-        $match: {
-          deprecated: false,
-        },
-      }, {
-        $group: {
-          _id: '$coingecko.id',
-        },
-      }, {
-        $sort: {
-          _id: 1,
-        },
-      }, {
-        $skip: PER_PAGE * page,
-      }, {
-        $limit: PER_PAGE,
-      }])
-      .toArray();
-    logs.push(`crypto update prices - load from db (chunk #${page}) - finish`);
-
-    if (cryptos.length === 0) {
-      break;
-    }
-
-    logs.push(`crypto update prices - load prices from coingecko (chunk #${page}) - start`);
-    const { data } = await coingecko.get('/simple/price', {
-      params: {
-        ids: cryptos.map(item => item._id).filter(item => !!item).join(','),
-        vs_currencies: CURRENCIES.join(','),
-      },
-    });
-    logs.push(`crypto update prices - load prices from coingecko (chunk #${page}) - finish`);
-
-    const operations = [];
-    const updatedAt = new Date();
-
-    for (const coingeckoId in data) {
-      const prices = {};
-      for (const currency in data[coingeckoId]) {
-        prices[currency.toUpperCase()] = data[coingeckoId][currency];
-      }
-      operations.push({
-        updateMany: {
-          filter: { 'coingecko.id': coingeckoId },
-          update: {
-            $set: {
-              prices,
-              'updated_at.prices': updatedAt,
-            },
+  try {
+    do {
+      logs.push(`crypto update prices - load from db (chunk #${page}) - start`);
+      cryptos = await db.collection(COLLECTION)
+        .aggregate([{
+          $match: {
+            deprecated: false,
           },
+        }, {
+          $group: {
+            _id: '$coingecko.id',
+          },
+        }, {
+          $sort: {
+            _id: 1,
+          },
+        }, {
+          $skip: PER_PAGE * page,
+        }, {
+          $limit: PER_PAGE,
+        }])
+        .toArray();
+      logs.push(`crypto update prices - load from db (chunk #${page}) - finish`);
+
+      if (cryptos.length === 0) {
+        break;
+      }
+
+      logs.push(`crypto update prices - load prices from coingecko (chunk #${page}) - start`);
+      const { data } = await coingecko.get('/simple/price', {
+        params: {
+          ids: cryptos.map(item => item._id).filter(item => !!item).join(','),
+          vs_currencies: CURRENCIES.join(','),
         },
       });
-      //console.log(`updated crypto prices coingecko id: ${coingeckoId}`);
-    }
+      logs.push(`crypto update prices - load prices from coingecko (chunk #${page}) - finish`);
 
-    logs.push(`crypto update prices - update db (chunk #${page}) - start`);
-    if (operations.length > 0) {
-      await db.collection(COLLECTION)
-        .bulkWrite(operations, { ordered: false });
-    }
-    logs.push(`crypto update prices - update db (chunk #${page}) - finish`);
+      const operations = [];
+      const updatedAt = new Date();
 
-    page++;
-  } while (cryptos.length === PER_PAGE);
+      for (const coingeckoId in data) {
+        const prices = {};
+        for (const currency in data[coingeckoId]) {
+          prices[currency.toUpperCase()] = data[coingeckoId][currency];
+        }
+        operations.push({
+          updateMany: {
+            filter: { 'coingecko.id': coingeckoId },
+            update: {
+              $set: {
+                prices,
+                'updated_at.prices': updatedAt,
+              },
+            },
+          },
+        });
+        //console.log(`updated crypto prices coingecko id: ${coingeckoId}`);
+      }
 
-  clearTimeout(timeout);
+      logs.push(`crypto update prices - update db (chunk #${page}) - start`);
+      if (operations.length > 0) {
+        await db.collection(COLLECTION)
+          .bulkWrite(operations, { ordered: false });
+      }
+      logs.push(`crypto update prices - update db (chunk #${page}) - finish`);
+
+      page++;
+    } while (cryptos.length === PER_PAGE);
+  } finally {
+    clearTimeout(timeout);
+  }
   console.log('crypto update prices - fineshed');
 }
 
@@ -142,64 +144,66 @@ async function updateRank() {
   let list;
   let map = [];
 
-  do {
-    logs.push(`crypto update rank - load from coinmarketcap (chunk #${page}) - start`);
-    const res = await coinmarketcap.get('/v1/cryptocurrency/map', {
-      params: {
-        listing_status: 'active,inactive,untracked',
-        limit: PER_PAGE,
-        start: (page * PER_PAGE) + 1,
-      },
-    });
-    logs.push(`crypto update rank - load from coinmarketcap (chunk #${page}) - finish`);
-    list = res.data.data;
-    page++;
-    map = map.concat(list);
-  } while (list.length === PER_PAGE);
+  try {
+    do {
+      logs.push(`crypto update rank - load from coinmarketcap (chunk #${page}) - start`);
+      const res = await coinmarketcap.get('/v1/cryptocurrency/map', {
+        params: {
+          listing_status: 'active,inactive,untracked',
+          limit: PER_PAGE,
+          start: (page * PER_PAGE) + 1,
+        },
+      });
+      logs.push(`crypto update rank - load from coinmarketcap (chunk #${page}) - finish`);
+      list = res.data.data;
+      page++;
+      map = map.concat(list);
+    } while (list.length === PER_PAGE);
 
-  logs.push('crypto update rank - load from db - start');
-  const updatedAt = new Date();
-  const cursor = db.collection(COLLECTION)
-    .aggregate([{
-      $match: {
-        deprecated: false,
-      },
-    }, {
-      $group: {
-        _id: '$coinmarketcap.id',
-      },
-    }, {
-      $sort: {
-        _id: 1,
-      },
-    }]);
-  const operations = [];
+    logs.push('crypto update rank - load from db - start');
+    const updatedAt = new Date();
+    const cursor = db.collection(COLLECTION)
+      .aggregate([{
+        $match: {
+          deprecated: false,
+        },
+      }, {
+        $group: {
+          _id: '$coinmarketcap.id',
+        },
+      }, {
+        $sort: {
+          _id: 1,
+        },
+      }]);
+    const operations = [];
 
-  for await (const cmc of cursor) {
-    const info = map.find((item) => item.id === cmc._id);
-    operations.push({
-      updateMany: {
-        filter: { 'coinmarketcap.id': cmc._id },
-        update: {
-          $set: {
-            rank: (info && info.rank) || Infinity,
-            'updated_at.rank': updatedAt,
+    for await (const cmc of cursor) {
+      const info = map.find((item) => item.id === cmc._id);
+      operations.push({
+        updateMany: {
+          filter: { 'coinmarketcap.id': cmc._id },
+          update: {
+            $set: {
+              rank: (info && info.rank) || Infinity,
+              'updated_at.rank': updatedAt,
+            },
           },
         },
-      },
-    });
-    //console.log(`updated crypto rank coinmarketcap id: ${cmc._id}`);
-  }
-  logs.push('crypto update rank - load from db - finish');
+      });
+      //console.log(`updated crypto rank coinmarketcap id: ${cmc._id}`);
+    }
+    logs.push('crypto update rank - load from db - finish');
 
-  logs.push('crypto update rank - update db - start');
-  if (operations.length > 0) {
-    await db.collection(COLLECTION)
-      .bulkWrite(operations, { ordered: false });
+    logs.push('crypto update rank - update db - start');
+    if (operations.length > 0) {
+      await db.collection(COLLECTION)
+        .bulkWrite(operations, { ordered: false });
+    }
+    logs.push('crypto update rank - update db - finish');
+  } finally {
+    clearTimeout(timeout);
   }
-  logs.push('crypto update rank - update db - finish');
-
-  clearTimeout(timeout);
   console.log('crypto update rank - finished');
 }
 
