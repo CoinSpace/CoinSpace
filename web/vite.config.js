c660c448df4251bb4080931b78776bd8a5e2d8ee
nikashitsa <vernik91@gmail.com>
2023-09-14 22:46:39 +0200

    chore: simplify vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -11,7 +11,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, './');
   return {
     server: {
       open: true,
@@ -58,8 +58,8 @@ export default defineConfig(({ mode }) => {
       sourcemap: 'hidden',
       rollupOptions: {
         input: {
-          main: path.resolve(__dirname, 'index.html'),
-          fido: path.resolve(__dirname, 'fido/index.html'),
+          main: './index.html',
+          fido: './fido/index.html',
         },
         output: {
           entryFileNames: 'assets/js/[name]-[hash].js',
