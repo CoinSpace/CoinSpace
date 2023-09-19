6f38917eb3a5dec84b670bae3a5fc06ade39dc5f
nikashitsa <vernik91@gmail.com>
2023-09-19 23:01:52 +0200

    feat(phonegap): add AppReview plugin

--- web/src/views/Crypto/Send/CryptoSendStepStatus.vue
+++ web/src/views/Crypto/Send/CryptoSendStepStatus.vue
@@ -1,12 +1,19 @@
 <script>
 import CsStep from '../../../components/CsStep.vue';
 import CsTransactionStatus from '../../../components/CsTransactionStatus.vue';
+import { onShowOnHide } from '../../../lib/mixins.js';
 
 export default {
   components: {
     CsTransactionStatus,
   },
   extends: CsStep,
+  mixins: [onShowOnHide],
+  onShow() {
+    if (this.storage.status && ['ios', 'android-play'].includes(this.env.VITE_DISTRIBUTION)) {
+      window.cordova.plugins.AppReview.requestReview().catch(() => {});
+    }
+  },
 };
 </script>
 
