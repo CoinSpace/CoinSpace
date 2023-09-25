58a7dc96f7d3e64f7bd7fe59e74e745f400b3c08
Evgeny Vlasenko <mahnunchik@gmail.com>
2023-09-25 13:38:14 -0600

    fix(ci): add PLATFORM env var

--- electron/build.js
+++ electron/build.js
@@ -24,29 +24,8 @@ function forgePlatform(distribution) {
   }
 }
 
-function vitePlatform(distribution) {
-  switch (distribution) {
-    case 'mac':
-      return 'mac';
-    case 'mas':
-    case 'mas-dev':
-      return 'mas';
-    case 'appx':
-    case 'appx-dev':
-      return 'win';
-    case 'snap':
-      return 'linux';
-    default:
-      return distribution;
-  }
-}
-
 console.log(`Start build (electron:${VITE_DISTRIBUTION})...`);
 execSync(`npm run publish -- --platform=${forgePlatform(VITE_DISTRIBUTION)}`, {
   stdio: [0, 1, 2],
-  env: {
-    ...process.env,
-    VITE_PLATFORM: vitePlatform(VITE_DISTRIBUTION),
-  },
 });
 console.log(`Done build  (electron:${VITE_DISTRIBUTION})`);
