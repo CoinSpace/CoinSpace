0f3b94a1cee8f4a49f4af5ee2018125a767f9ead
Evgeny Vlasenko <mahnunchik@gmail.com>
2024-01-26 13:11:35 -0600

    feat(server): cleanup

--- server/middleware.js
+++ server/middleware.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import crypto from 'crypto';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
-import querystring from 'querystring';
 
 function init(app) {
 
@@ -27,30 +25,6 @@ function init(app) {
       req.bodyHash = req.body ? crypto.createHash('sha256').update(buf).digest('hex') : '';
     },
   }));
-
-  app.use('/api/', (req, res, next) => {
-    const id = req.query.id || req.body.deviceId || req.body.id || req.body.wallet_id;
-    if (!id || !/^[a-f0-9]{64}$/.test(id)) return next();
-    const [, app, store, version] = req.get('X-Release') !== undefined ?
-      req.get('X-Release').match(/(.+)\.(.+)@(.+)/i) || [] : [];
-    const screen = req.baseUrl + req.path + (req.query.crypto ? `/${req.query.crypto}` : '')
-      + (req.query.network ? `/${req.query.network}` : '');
-    const useragent = req.get('User-Agent');
-    const language = req.acceptsLanguages()[0];
-    next();
-    axios({
-      url: 'https://www.google-analytics.com/collect',
-      method: 'post',
-      data: querystring.stringify({
-        v: 1, t: 'screenview',
-        tid: process.env.ANALYTICS_ID,
-        aip: 1, uid: id,
-        dl: screen, dt: screen, cd: screen,
-        uip: req.ip, ua: useragent, ul: language,
-        an: app, av: version, aiid: store,
-      }),
-    }).catch(() => {});
-  });
 
   const cacheControl = isProduction() ? { maxAge: dayInMs, setHeaders: setCustomCacheControl } : null;
   app.use(express.static(fileURLToPath(new URL('dist', import.meta.url)), cacheControl));
