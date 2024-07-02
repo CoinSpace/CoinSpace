3757eeb557be13e1066541729a565d44abb430b7
nikashitsa <vernik91@gmail.com>
2024-07-02 11:27:52 +0200

    feat: bring back ramps for ios & mas

--- web/src/lib/app.js
+++ web/src/lib/app.js
@@ -8,7 +8,7 @@ export function createApp({ App, router }) {
   router.$app = app;
 
   defineAppProperty(app, 'env', import.meta.env);
-  defineAppProperty(app, '$showRampsAndExchange', !['mas', 'ios'].includes(import.meta.env.VITE_DISTRIBUTION));
+  defineAppProperty(app, '$showRampsAndExchange', true);
   defineAppProperty(app, '$safeOpen', safeOpen);
   defineAppProperty(app, '$c', function(value, style = 'currency', options = {}) {
     if (!value) {
