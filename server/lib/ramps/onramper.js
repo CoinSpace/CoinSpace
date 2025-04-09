e3d7a93cf7957a806d8db3fe3ca90e83a9de1319
Evgeny Vlasenko <mahnunchik@gmail.com>
2025-04-09 12:12:19 +0100

    feat(server): onramper sign

--- server/lib/ramps/onramper.js
+++ server/lib/ramps/onramper.js
@@ -1,9 +1,32 @@
+import crypto from 'node:crypto';
+
 const API_KEY = process.env.ONRAMPER_API_KEY;
+const SIGNING_KEY = process.env.ONRAMPER_SIGNING_KEY;
 const rampData = {
   id: 'onramper',
   name: 'Onramper',
   description: 'Aggregator',
 };
+
+function signUrl(url) {
+  if (!SIGNING_KEY) return url;
+  const sensitiveParams = new URLSearchParams();
+  for (const [key, value] of url.searchParams.entries()) {
+    if (['wallets', 'walletAddressTags', 'networkWallets'].includes(key)) {
+      sensitiveParams.append(key, value);
+    }
+  }
+  if (sensitiveParams.size === 0) return url;
+  // simplified sorting because we use only single key:value param
+  // https://docs.onramper.com/docs/signing-widget-url
+  sensitiveParams.sort();
+  const signature = crypto
+    .createHmac('sha256', SIGNING_KEY)
+    .update(decodeURIComponent(sensitiveParams.toString()))
+    .digest('hex');
+  url.searchParams.append('signature', signature);
+  return url;
+}
 
 function ramp(type, { crypto, address }) {
   if (!API_KEY) return;
@@ -25,7 +48,7 @@ function ramp(type, { crypto, address }) {
   }
   return {
     ...rampData,
-    url: url.toString(),
+    url: signUrl(url).toString(),
   };
 }
 
